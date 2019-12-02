a33957d3e00411a70da7947a9f9f4977a70af9ae
henkewahlstrom <57446746+henkewahlstrom@users.noreply.github.com>
2019-12-02 15:45:12 +0100

    Add files via upload

---
+++ public/js/menu.js
@@ -0,0 +1,22 @@
+var food = [
+  {name: "Umami burger",
+   kCal: "500 kCal",
+  lactose: false,
+  gluten: true,
+  stock: 70,
+  image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTEEwenBuBCeAaFC-FptIF-NGBDCUoRF-FTeWR5fF_bdBaURmGH"},
+
+  {name: "Bean burger",
+   kCal: "300 kCal",
+   lactose: true,
+   gluten: true,
+   stock: 2,
+   image: "https://www.jessicagavin.com/wp-content/uploads/2018/05/vegan-black-bean-burgers-5-1200.jpg"},
+
+  {name: "Canadian burger",
+  kCal: "1000 kCal",
+  lactose: false,
+  gluten: true,
+  stock: 20,
+  image: "https://extremecouponingmom.ca/wp-content/uploads/2019/08/Great-Canadian-Turkey-Burger_7-720x720.jpg"}
+]
